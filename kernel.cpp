f62a119d20c37ad610b97fbe7f24fe9b530f2977
dylngg <dylan@gardnermedia.com>
2021-04-10 17:21:28 -0600

    Kernel: Add minimal shell with single command to report mem usage
    This commit uses the new UART recieve cabilities to create a console
    shell. Presently this shell only supports one command: memstat, which
    reports on the amount of memory used in the kernel.

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,68 @@
-#include "kmalloc.hpp"
 #include "console.hpp"
+#include "kmalloc.hpp"
 
 extern "C" [[noreturn]] void init();
 
-void init()
+static void readline(char* buf, size_t bufsize)
 {
+    size_t offset = 0;
+    char ch;
+
+    for (;;) {
+        ch = console_get();
+        if (ch == '\r')
+            break;
+        if (ch == '\n')
+            break;
+
+        if (offset >= bufsize - 1)
+            continue;
+
+        // For some reason local echo doesn't work on my machine, so we'll just
+        // echo it here...
+        console_put(ch);
+
+        buf[offset] = ch;
+        offset++;
+    }
+
+    console_put('\n');
+    buf[offset] = '\0';
+    return;
+}
+
+static void builtin_memstat()
+{
+    auto malloc_stats = kmemstats();
+    int pct_mem_util = (malloc_stats.amount_used * 100) / malloc_stats.heap_size;
+    consolef("heap size: %d bytes\nused: %d bytes (%d%% util)\nnmallocs: %d\nnfrees: %d\n",
+        malloc_stats.heap_size, malloc_stats.amount_used, pct_mem_util, malloc_stats.num_mallocs,
+        malloc_stats.num_frees);
+}
+
+static void start_shell()
+{
+    char* buf = (char*)kmalloc(1024);
+
+    for (;;) {
+        console("# ");
+        readline(buf, 1024);
+
+        if (strcmp(buf, "exit") == 0)
+            break;
+        if (strcmp(buf, "memstat") == 0) {
+            builtin_memstat();
+            continue;
+        }
+        consolef("Unknown command '%s'\n", buf);
+    }
+
+    kfree((void*)buf);
+}
+
+[[noreturn]] void init()
+{
+    console_init();
     consoleln("Initializing... ");
     kmalloc_init();
 
@@ -19,6 +77,9 @@ void init()
     kfree((void*)pinyon);
     kfree((void*)pine);
 
+    start_shell();
+
+    consoleln("goodbye.");
     while (1) {
     }
 }
--- kmalloc.hpp
+++ kmalloc.hpp
@@ -6,3 +6,12 @@ void kmalloc_init();
 void kfree(void*);
 
 void* kmalloc(size_t) __attribute__((malloc));
+
+struct KMallocStats {
+    size_t heap_size;
+    size_t amount_used;
+    u16 num_mallocs;
+    u16 num_frees;
+};
+
+KMallocStats kmemstats();
