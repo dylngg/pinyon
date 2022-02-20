7cc7306fbfa6bcf66307aa6037289ccb49fc3521
dylngg <dylan@gardnermedia.com>
2022-02-20 00:52:49 -0700

    Make units constexpr instead of macros

--- pine/units.hpp
+++ pine/units.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#define KiB 1024
-#define MiB 1048576
-#define GiB 1073741824
-#define Page 4096
+constexpr unsigned KiB = 1024;
+constexpr unsigned MiB = 1048576;
+constexpr unsigned GiB = 1073741824;
+constexpr unsigned Page = 4 * KiB;
+constexpr unsigned Alignment = 4;
 
 #define SYS_HZ 8
 #define SYS_HZ_BITS 3
