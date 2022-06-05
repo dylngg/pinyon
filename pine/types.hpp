3970755517ff8a87b316a93a3399473e6cedad36
dylngg <dylan@gardnermedia.com>
2022-06-04 21:20:40 -0600

    Remove unnecessary comment

--- pine/types.hpp
+++ pine/types.hpp
@@ -15,7 +15,6 @@ using i32 = int32_t;
 using i16 = int16_t;
 using i8 = int8_t;
 
-// ARCH: 32-bit dependent
 // We do necessary pointer arithmetic that we cannot do with void* unless
 // we're ok with the compiler constantly screaming at us.
 using PtrData = uintptr_t;
