fceede5edb4593bfa87b38bedb8ee5d34bd0e2b1
dylngg <dylan@gardnermedia.com>
2022-06-04 21:20:40 -0600

    Prevent underflow in memcpy, memset and bzero
    How embarrassing...

--- pine/c_builtins.cpp
+++ pine/c_builtins.cpp
@@ -3,18 +3,21 @@
 void bzero(void* target, size_t size)
 {
     unsigned char* _target = (unsigned char*)target;
-    while (size-- > 0)
+    while (size > 0) {
         *_target = 0;
+        size--;
+    }
 }
 
 void* memcpy(void* __restrict__ to, const void* __restrict__ from, size_t size)
 {
     unsigned char* _to = (unsigned char*)to;
     unsigned char* _from = (unsigned char*)from;
-    while (size-- > 0) {
+    while (size > 0) {
         *_to = *_from;
         _to++;
         _from++;
+        size--;
     }
 
     return to;
@@ -24,8 +27,10 @@ void* memset(void* to, int c, size_t size)
 {
     unsigned char* _to = (unsigned char*)to;
     unsigned char c_ = (unsigned char)c;
-    while (size-- > 0)
+    while (size > 0) {
         *_to++ = c_;
+        size--;
+    }
 
     return to;
 }
